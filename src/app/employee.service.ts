614547f1b1f657555c74bb4bfc7d78ba996637a7
LOKESH <lokesh.prasad@sap.com>
2024-09-20 18:21:13 +0530

    minor

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://localhost:8080/api/v1/employees";
+  private baseURL = "https://employeedetailsbackend.cfapps.us10-001.hana.ondemand.com/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
   
